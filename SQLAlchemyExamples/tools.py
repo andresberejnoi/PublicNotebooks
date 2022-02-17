6e472e5dc3c9f942aa63fa947883dcc801490ae2
Andres Berejnoi <andresberejnoi@gmail.com>
2022-02-17 12:37:02 -0500

    if databse is sqlite, create an sqlite db in memory

--- SQLAlchemyExamples/tools.py
+++ SQLAlchemyExamples/tools.py
@@ -14,6 +14,8 @@ def generate_uri_from_file(config_file='db_config.yml'):
     port      = config.get('port')
     db_name   = config.get('db_name')
 
+    if database == 'sqlite':
+        return "sqlite://"  #this will keep the database in RAM while program lives
     database_uri = f"{database}://{username}:{password}@{host}:{port}/{db_name}"
     return database_uri
 
