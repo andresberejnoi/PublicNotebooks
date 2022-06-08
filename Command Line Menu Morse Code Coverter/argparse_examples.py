80a3df1b37b5ae8da6b6f2b8381eaeebe4ac6f8e
Andres <andresberejnoi@gmail.com>
2022-06-08 18:14:16 -0400

    added description to ArgumentParser object

--- Command Line Menu Morse Code Coverter/argparse_examples.py
+++ Command Line Menu Morse Code Coverter/argparse_examples.py
@@ -3,13 +3,28 @@ import datetime as dt
 
 
 def cli():
-    parser = argparse.ArgumentParser(prog='Simple Example')
+    parser = argparse.ArgumentParser(description='Simple Example')
     parser.add_argument('-n', '--name', type=str, default='John', help="Name of user")
     parser.add_argument('-a', '--age', type=int, default=31, help="Age of user")
     parser.add_argument('-o', '--occupation', type=str, default='software developer', help='Occupation of user')
     
     args = parser.parse_args()
     return args
+
+
+def fuller_cli():
+    parser = argparse.ArgumentParser(description='Example Program')
+
+    parser.add_argument('-v','--verbose',action='store_true')
+    parser.add_argument('--coordinates', type=float, nargs=2,)
+    parser.add_argument('--trading-pair', type=str, defautl='BTC-ETH', choices=['BTC-ETH', 'BTC-USD', 'ETH-USD', 'ADA-USD'])
+
+    args = parser.parse_args()
+    return args
+
+def fuller_main(args):
+    print()
+    print(f"Coordinates: {args.coordinates}")
 
 
 def main(args):
@@ -23,5 +38,5 @@ def main(args):
 
 
 if __name__ == "__main__":
-    args = cli()
-    main(args)
+    args = fuller_cli()
+    fuller_main(args)
