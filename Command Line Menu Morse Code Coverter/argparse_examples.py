44e2cb16c0f0f62a36c564f8b1cdef41595d9636
Andres <andresberejnoi@gmail.com>
2022-06-08 12:37:49 -0400

    improved defautls for the example

--- Command Line Menu Morse Code Coverter/argparse_examples.py
+++ Command Line Menu Morse Code Coverter/argparse_examples.py
@@ -4,10 +4,10 @@ import datetime as dt
 
 def cli():
     parser = argparse.ArgumentParser(prog='Simple Example')
-    parser.add_argument('-n', '--name', type=str, default=None, help="Name of user")
-    parser.add_argument('-a', '--age', type=int, help="Age of user")
+    parser.add_argument('-n', '--name', type=str, default='John', help="Name of user")
+    parser.add_argument('-a', '--age', type=int, default=31, help="Age of user")
+    parser.add_argument('-o', '--occupation', type=str, default='software developer', help='Occupation of user')
     
-    parser.add_argument('-o', '--occupation', type=str, default='Fly catcher', help='Occupation of user')
     args = parser.parse_args()
     return args
 
